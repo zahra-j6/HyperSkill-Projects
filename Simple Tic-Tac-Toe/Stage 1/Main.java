d4ce7c93ca5ff397b10aeaedc324b14b4a5e080b
Zahra Jalia <55066334+zahra-j6@users.noreply.github.com>
2022-06-26 15:41:48 -0400

    Create Main.java

---
+++ Simple Tic-Tac-Toe/Stage 1/Main.java
@@ -0,0 +1,8 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("X O X\nO X O\nX X O ");
+    }
+}
