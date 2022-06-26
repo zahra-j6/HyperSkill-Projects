063c5f248db25f67331bf1b35b3ca743932ef891
Zahra Jalia <55066334+zahra-j6@users.noreply.github.com>
2022-06-26 10:51:35 -0400

    Create SimpleBot.java

---
+++ Simple Chatty Bot/Simple Chatty Bot/Hello! What’s your name/SimpleBot.java
@@ -0,0 +1,8 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello! My name is Aid.\nI was created in 2020.");
+    }
+}
