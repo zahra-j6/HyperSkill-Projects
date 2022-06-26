cfea7b902486e35d051c983e39d589d4363ac70e
Zahra Jalia <55066334+zahra-j6@users.noreply.github.com>
2022-06-26 11:22:48 -0400

    Create Cinema.java

---
+++ Cinema Room Manager/Stage 1/Cinema.java
@@ -0,0 +1,13 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for( int i = 1 ;i< 8 ;i++){
+            System.out.println(i + " S S S S S S S S");
+        }
+    }
+}
