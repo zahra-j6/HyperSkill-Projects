0df1f56cdb19c2e743cece756cc99aeb15bbed5c
Zahra Jalia <55066334+zahra-j6@users.noreply.github.com>
2022-06-27 13:39:10 -0400

    Create Main.java

---
+++ Bulls and Cows/Stage 1/Main.java
@@ -0,0 +1,7 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.\nTurn 1. Answer:\n1234\nGrade: None.\n\nTurn 2. Answer:\n9876\nGrade: 4 bulls.\nCongrats! The secret code is 9876.");
+    }
+}
