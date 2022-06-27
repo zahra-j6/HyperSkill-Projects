bdf4ab6e49c7795000a96cda461b94ccd87fd16a
Zahra Jalia <55066334+zahra-j6@users.noreply.github.com>
2022-06-26 23:53:55 -0400

    Create Main.java

---
+++ Zookeeper/Zookeeper/Rush into print/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+                    System.out.println("I love animals!\nLet's check on the animals...\nThe deer looks fine.\nThe bat looks happy.\nThe lion looks healthy.");
+    }
+}
